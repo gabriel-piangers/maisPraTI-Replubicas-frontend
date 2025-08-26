9aa26df38081268223abe98801bec6683ae1ef49
Edimaique <edimaiqueacacio@gmail.com>
2025-08-26 19:34:41 -0300

    feat: adicionar rota /register para a tela de cadastro

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes,  } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import "./styles/index.css"
+import RegisterPage from './pages/RegisterPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index path='/' element={<HomePage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
