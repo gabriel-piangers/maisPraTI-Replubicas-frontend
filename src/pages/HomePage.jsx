ebffa39534d4080eff91d3821606847806d7453a
gabriel-piangers <piangersgabriel@gmail.com>
2025-08-29 19:26:26 -0300

    Modificado a estilização da HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,21 @@
-import { Header } from '../components/Header'
-import { FirstSection } from '../components/firstSection'
-import { AboutCards } from '../components/AboutCards'
-import { Footer } from '../components/Footer'
-import { ExploreCategories } from '../components/ExploreCategories'
-import { FeaturedProperties } from '../components/FeaturedProperties'
-
-
-
+import { Header } from "../components/Header";
+import { FirstSection } from "../components/firstSection";
+import { AboutCards } from "../components/AboutCards";
+import { Footer } from "../components/Footer";
+import { ExploreCategories } from "../components/ExploreCategories";
+import { FeaturedProperties } from "../components/FeaturedProperties";
 
 export function HomePage() {
   return (
     <>
       <Header />
-      <FirstSection />
-      <ExploreCategories />
-      <FeaturedProperties />
-      <AboutCards />
+      <FirstSection />  
+      <main className="homepage-main">
+        <ExploreCategories />
+        <FeaturedProperties />
+        <AboutCards />
+      </main>
       <Footer />
     </>
-  )
+  );
 }
