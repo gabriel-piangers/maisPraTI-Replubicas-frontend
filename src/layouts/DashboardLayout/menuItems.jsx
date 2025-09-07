335e89d99ef9a2f5ca3133dbd5f366801655cebd
diogoscsilva <diogosantoro@gmail.com>
2025-09-07 18:10:02 -0300

    rsolve routerAPP e sideBarMaenuo para resolver conflito

---
+++ src/layouts/DashboardLayout/menuItems.jsx
@@ -0,0 +1,47 @@
+import {
+  FaUsers,
+  FaHome,
+  FaBell,
+  FaCog,
+  FaTasks,
+  FaWallet,
+  FaBed,
+} from "react-icons/fa";
+
+export const menuItems = [
+  {
+    key: "/", // 
+    label: "Boas-vindas",
+    icon: <FaHome />,
+  },
+  {
+    key: "/dashboard/ads", 
+    label: "Meus Anúncios",
+    icon: <FaBell />,
+  },
+  {
+    key: "./residents",
+    label: "Moradores",
+    icon: <FaUsers />,
+  },
+  {
+    key: "./rooms",
+    label: "Quartos",
+    icon: <FaBed />,
+  },
+  {
+    key: "./expenses",
+    label: "Despesas",
+    icon: <FaWallet />,
+  },
+  {
+    key: "./tasks",
+    label: "Tarefas",
+    icon: <FaTasks />,
+  },
+  {
+    key: "/dashboard/settings", 
+    label: "Configurações",
+    icon: <FaCog />,
+  },
+];
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
+
+import { AuthLayout } from "../layouts/AuthLayout";
+import { DashboardLayout } from "../layouts/DashboardLayout";
+import { ResidentsPage } from "../pages/ResidentsPage";
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthLayout showHeader />}>
+          <Route index element={<HomePage />} />
+          <Route path="dashboard" element={<DashboardLayout />}>
+            <Route path="residents" element={<ResidentsPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
