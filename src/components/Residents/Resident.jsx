d90daf6c4292b4224d972ccf070ac488c8632023
diogoscsilva <diogosantoro@gmail.com>
2025-09-25 21:30:54 -0300

    Form com inputs não controlados com submitHandlers, extração do useEffect que carrega os dados mock do constructor do objeto DataModel (executava a cada chamada de qualquer hook do model) para o modelprovider (só executa 1 vez).

--- src/components/Residents/Resident.jsx
+++ src/components/Residents/Resident.jsx
@@ -12,32 +12,42 @@ export function Resident(props) {
 
     const [editMode, setEditMode] = useState(false)
     const { editItem, removeItem } = props
-    const [administrator, setAdministrator] = useState(props.administrator)
+
+    const resident = props.resident
 
     const model = useContext(ModelContext)
     const [rooms, roomsModel] = model.roomHook(props.resident.name)
-    function removeFromRoom () {
-        const removedIndex = rooms[index].residents.indexOf(props.resident.name)
-        rooms[index].residents.splice(removedIndex, 1)
-        roomsModel.set(rooms)
+
+    function removeFromRoom() {
+        roomsModel.update(index, {room: -1})
     }
-    function setRoom (inputs) {
+
+    function setRoom(inputs) {
         roomsModel.update(index, inputs)
     }
-    
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const room = formData.get('room')
+        formData.delete('room')
+        const payload = Object.fromEntries(formData.entries())
+        payload.administrator = payload.hasOwnProperty("administrator") ? true : false
+        editItem(payload)
+        setRoom({ room })
+        setEditMode(false)
+    }
+
+
     const index = rooms.findIndex(room => {
         return room.residents.includes(props.resident.name)
     })
-
-    const [roomIndex, setRoomIndex] = useState(-1)
-
-    useMemo(() => setRoomIndex(index), [rooms])
 
     function selectRoom() {
         return (
             <>
                 <Text type="secondary">Quarto </Text>
-                <select value={roomIndex} name='room' onChange={e => setRoomIndex(e.target.value)} >
+                <select defaultValue={index} name='room' >
                     <option value={-1} name='room' >
                         nenhum
                     </option>
@@ -55,74 +65,69 @@ export function Resident(props) {
     }
 
     return (
-        <>
+        <form className="resident-card-container" onSubmit={handleFormSubmit}>
             <div className="resident-card-content">
 
-                <>
-                    <Flex gap={16} align="center" wrap>
-                        <Avatar className="resident-avatar" size={48}>
-                            {getInitial(props.resident.name)}
-                        </Avatar>
-                        <Flex vertical>
-                            <Text strong className="resident-name">
-                                {props.resident.name}
-                            </Text>
-                            <Space size="middle">
-                                {
-                                    editMode
-                                        ? selectRoom()
-                                        : <>
-                                            <Text type="secondary">
-                                                Quarto {(index + 1) || 'nenhum'}
-                                            </Text>
+                <Flex gap={16} align="center" wrap>
+                    <Avatar className="resident-avatar" size={48}>
+                        {getInitial(resident.name)}
+                    </Avatar>
+                    <Flex vertical>
+                        <Text strong className="resident-name">
+                            {resident.name}
+                        </Text>
+                        <Space size="middle">
+                            {
+                                editMode
+                                    ? selectRoom()
+                                    : <>
+                                        <Text type="secondary">
+                                            Quarto {(index + 1) || 'nenhum'}
+                                        </Text>
+                                        <Text type="secondary">•</Text>
+                                        <Text type="secondary">{resident.telefone}</Text>
+                                    </>
+                            }
+                            {
+                                editMode
+                                    ? <>
+                                        <Text type="secondary">Administrador: </Text>
+                                        <input type="checkbox" defaultChecked={resident.administrator} name='administrator' />
+                                    </>
+                                    : resident.administrator
+                                    && (
+                                        <>
                                             <Text type="secondary">•</Text>
-                                            <Text type="secondary">{props.resident.telefone}</Text>
-                                        </>
-                                }
-                                {
-                                    editMode
-                                        ? <>
-                                            <Text type="secondary">Administrador: </Text>
-                                            <input type="checkbox" name='administrator' onChange={e => setAdministrator(e.target.value)}/>
-                                        </>
-                                        : props.resident.administrator
-                                        && (
-                                            <>
-                                                <Text type="secondary">•</Text>
-                                                <Text type="secondary">Administrador <FaCheck /></Text>
+                                            <Text type="secondary">Administrador <FaCheck /></Text>
 
-                                            </>
-                                        )
-                                }
-                            </Space>
+                                        </>
+                                    )
+                            }
+                        </Space>
+                    </Flex>
+                </Flex>
+
+                {
+                    !editMode
+                    && <Flex align="center" gap={32}>
+                        <Flex vertical align="end">
+                            <span>
+                                <Text strong className="resident-value">
+                                    Saldo:
+                                </Text>
+
+                                <Text>
+                                    {' R$'} <Balance residentName={props.resident.name} />
+                                </Text>
+                            </span>
                         </Flex>
                     </Flex>
-
-                    {
-                        !editMode
-                        && <Flex align="center" gap={32}>
-                            <Flex vertical align="end">
-                                <span>
-                                    <Text strong className="resident-value">
-                                        Saldo:
-                                    </Text>
-
-                                    <Text>
-                                        {' R$'} <Balance residentName={props.resident.name} />
-                                    </Text>
-                                </span>
-                            </Flex>
-                        </Flex>
-                    }
-                </>
+                }
             </div>
 
             {
                 editMode
-                    ? <EditButtons editItem={() => {
-                            setRoom({room: roomIndex})
-                            editItem({administrator})
-                        }}
+                    ? <EditButtons
                         removeItem={() => {
                             removeFromRoom()
                             removeItem()
@@ -130,7 +135,7 @@ export function Resident(props) {
                         setEditMode={setEditMode} />
                     : <EditButton setEditMode={setEditMode} />
             }
-        </>
+        </form>
 
     )
 }
